fc6c053051ce6dc06590a81879f500f9b285dbcb
Oleksii26 <Kolesnik26@ukr.net>
2022-09-05 11:12:04 +0300

    save

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,1 +1,20 @@
+import Player from '@vimeo/player';
+import throttle from 'lodash.throttle';
 
+const iframe = document.querySelector('iframe');
+const player = new Player(iframe);
+var throttle = require('lodash.throttle');
+
+const currentTimeUpdate = function (data) {
+  const currentTime = data.seconds;
+
+  localStorage.setItem('videoplayer-current-time', currentTime);
+};
+
+player.on('timeupdate', throttle(currentTimeUpdate, 1_000));
+
+const getCurrentTimeUpdate = localStorage.getItem('videoplayer-current-time');
+
+if (getCurrentTimeUpdate) {
+  player.setCurrentTime(getCurrentTimeUpdate);
+}
